741481f7c7902b20b31fb6c6b1961c10313350e7
Saeschu <33346813+Saeschu@users.noreply.github.com>
2017-11-20 23:22:56 +0100

    Imput Window
    Imput Window und symetrischer Sparrenabstand

--- Lagerhalle_o_Function.m
+++ Lagerhalle_o_Function.m
@@ -1,11 +1,30 @@
 clear all, clc
+%{
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 Dim=0.2         %Dimension der Stahltrager  - zurzeitn Quadrate
 l=20         %Länge
-b=14             %Breite zwischen den Stützen
+b=16           %Breite zwischen den Stützen
 h=3             %Höhe
 a=1.5          %Binderabstand
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%}
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Input Window
+prompt = {'Länge:','Breite:', 'Höhe', 'Abstand','Dim'};
+dlg_title = 'Input';
+num_lines = 1;
+defaultans = {'20','15','3','4','0.2'}; % Zahlenwerte können 'zwischen' entfert werden nur platzhalter
+answer = inputdlg(prompt,dlg_title,num_lines,defaultans);
+l= str2num(answer{1});
+b= str2num(answer{2});
+h= str2num(answer{3});
+a= str2num(answer{4});
+Dim= str2num(answer{5});
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
 %set axis and figure
 clf;
 figure(1)
@@ -52,22 +71,38 @@ vert8= [Dim (l-Dim) (h-Dim); Dim (l-Dim) h; Dim l h; Dim l (h-Dim); (b-Dim) l h;
 patch('Faces', fac,'Vertices',vert8,'FaceColor','g');            % Erzeugen der Gefärbten Flächen (Polygonen)
 
 %Binderi i Stützen und Querbalken
-i=0;
-while i <= l-a;         %alternatieve auch möglich i<= l-(2*a) um den letzten binder nicht darzustellen
+%Aufbau von der Mittea aus um Anfang und Endabstand Binder-Stütze gleich
+%gross zu halten
+i=-a;
+while i <= (l/2)-2*a;         
       i= i + a;
 %Querbalken 
-vert9= [Dim i (h-Dim); Dim i h; Dim (i+Dim) h; Dim (i+Dim) (h-Dim); (b-Dim) (i+Dim) h; (b-Dim) (i+Dim) (h-Dim); (b-Dim) i (h-Dim); (b-Dim) i h]; % [x y z] Eckpunkte der Träger in Vektorschreibweise
+vert9= [Dim ((l/2)+i-(Dim/2)) (h-Dim); Dim ((l/2)+i-(Dim/2)) h; Dim (((l/2)+i-(Dim/2))+Dim) h; Dim (((l/2)+i-(Dim/2))+Dim) (h-Dim); (b-Dim) (((l/2)+i-(Dim/2))+Dim) h; (b-Dim) (((l/2)+i-(Dim/2))+Dim) (h-Dim); (b-Dim) ((l/2)+i-(Dim/2)) (h-Dim); (b-Dim) ((l/2)+i-(Dim/2)) h]; % [x y z] Eckpunkte der Träger in Vektorschreibweise
 patch('Faces', fac,'Vertices',vert9,'FaceColor','y');            % Erzeugen der Gefärbten Flächen (Polygonen)
 
 %Stuetze A
-vert10= [0 i 0; 0 (i+Dim) 0; Dim (i+Dim) 0; Dim i 0; Dim (i+Dim) (h-Dim); Dim i (h-Dim); 0 i (h-Dim); 0 (i+Dim) (h-Dim)]; % [x y z] Eckpunkte der Träger in Vektorschreibweise
+vert10= [0 ((l/2)+i-(Dim/2)) 0; 0 (((l/2)+i-(Dim/2))+Dim) 0; Dim (((l/2)+i-(Dim/2))+Dim) 0; Dim ((l/2)+i-(Dim/2)) 0; Dim (((l/2)+i-(Dim/2))+Dim) (h-Dim); Dim ((l/2)+i-(Dim/2)) (h-Dim); 0 ((l/2)+i-(Dim/2)) (h-Dim); 0 (((l/2)+i-(Dim/2))+Dim) (h-Dim)]; % [x y z] Eckpunkte der Träger in Vektorschreibweise
 patch('Faces', fac,'Vertices',vert10,'FaceColor','y');            % Erzeugen der Gefärbten Flächen (Polygonen)
 
 %Stuetze B
-vert11= [b i 0; b (i+Dim) 0; (b-Dim) (i+Dim) 0; (b-Dim) i 0; (b-Dim) (i+Dim) (h-Dim); (b-Dim) i (h-Dim); b i (h-Dim); b (i+Dim) (h-Dim)]; % [x y z] Eckpunkte der Träger in Vektorschreibweise
+vert11= [b ((l/2)+i-(Dim/2)) 0; b (((l/2)+i-(Dim/2))+Dim) 0; (b-Dim) (((l/2)+i-(Dim/2))+Dim) 0; (b-Dim) ((l/2)+i-(Dim/2)) 0; (b-Dim) (((l/2)+i-(Dim/2))+Dim) (h-Dim); (b-Dim) ((l/2)+i-(Dim/2)) (h-Dim); b ((l/2)+i-(Dim/2)) (h-Dim); b (((l/2)+i-(Dim/2))+Dim) (h-Dim)]; % [x y z] Eckpunkte der Träger in Vektorschreibweise
 patch('Faces', fac,'Vertices',vert11,'FaceColor','y');            % Erzeugen der Gefärbten Flächen (Polygonen)
 end
+k=l/2;
+while k >= 2*a;         
+      k= k - a;
+%Querbalken 
+vert9= [Dim (k-(Dim/2)) (h-Dim); Dim (k-(Dim/2)) h; Dim ((k-(Dim/2))+Dim) h; Dim ((k-(Dim/2))+Dim) (h-Dim); (b-Dim) ((k-(Dim/2))+Dim) h; (b-Dim) ((k-(Dim/2))+Dim) (h-Dim); (b-Dim) (k-(Dim/2)) (h-Dim); (b-Dim) (k-(Dim/2)) h]; % [x y z] Eckpunkte der Träger in Vektorschreibweise
+patch('Faces', fac,'Vertices',vert9,'FaceColor','y');            % Erzeugen der Gefärbten Flächen (Polygonen)
 
+%Stuetze A
+vert10= [0 (k-(Dim/2)) 0; 0 ((k-(Dim/2))+Dim) 0; Dim ((k-(Dim/2))+Dim) 0; Dim (k-(Dim/2)) 0; Dim ((k-(Dim/2))+Dim) (h-Dim); Dim (k-(Dim/2)) (h-Dim); 0 (k-(Dim/2)) (h-Dim); 0 ((k-(Dim/2))+Dim) (h-Dim)]; % [x y z] Eckpunkte der Träger in Vektorschreibweise
+patch('Faces', fac,'Vertices',vert10,'FaceColor','y');            % Erzeugen der Gefärbten Flächen (Polygonen)
+
+%Stuetze B
+vert11= [b (k-(Dim/2)) 0; b ((k-(Dim/2))+Dim) 0; (b-Dim) ((k-(Dim/2))+Dim) 0; (b-Dim) (k-(Dim/2)) 0; (b-Dim) ((k-(Dim/2))+Dim) (h-Dim); (b-Dim) (k-(Dim/2)) (h-Dim); b (k-(Dim/2)) (h-Dim); b ((k-(Dim/2))+Dim) (h-Dim)]; % [x y z] Eckpunkte der Träger in Vektorschreibweise
+patch('Faces', fac,'Vertices',vert11,'FaceColor','y');            % Erzeugen der Gefärbten Flächen (Polygonen)
+end
 
 
 
