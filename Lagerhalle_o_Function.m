908d3dddb4bac4792d745d43c028e94e851fa18e
Saeschu <33346813+Saeschu@users.noreply.github.com>
2017-11-22 19:41:46 +0100

    Mit Binder und Stüzten in der Mitte

--- Lagerhalle_o_Function.m
+++ Lagerhalle_o_Function.m
@@ -15,16 +15,15 @@ a=1.5          %Binderabstand
 prompt = {'Länge:','Breite:', 'Höhe', 'Abstand','Dim'};
 dlg_title = 'Input';
 num_lines = 1;
-defaultans = {'20','15','3','4','0.2'}; % Zahlenwerte können 'zwischen' entfert werden nur platzhalter
+defaultans = {'30','20','3','4','0.2'}; % Zahlenwerte können 'zwischen' entfert werden nur platzhalter
 answer = inputdlg(prompt,dlg_title,num_lines,defaultans);
 l= str2num(answer{1});
 b= str2num(answer{2});
 h= str2num(answer{3});
 a= str2num(answer{4});
 Dim= str2num(answer{5});
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%
+
 %set axis and figure
 clf;
 figure(1)
@@ -36,6 +35,7 @@ grid on
 xlabel('x - Breite')
 ylabel('y - Länge')
 zlabel('z - Höhe')
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %Laengstraeger 1
 vert1= [0 0 (h-Dim); Dim 0 (h-Dim); Dim 0 h; 0 0 h; Dim l h; 0 l h; 0 l (h-Dim); Dim l (h-Dim)]; % [x y z] Eckpunkte der Träger in Vektorschreibweise
@@ -69,40 +69,79 @@ patch('Faces', fac,'Vertices',vert7,'FaceColor','g');            % Erzeugen der 
 %Querbalken Ende
 vert8= [Dim (l-Dim) (h-Dim); Dim (l-Dim) h; Dim l h; Dim l (h-Dim); (b-Dim) l h; (b-Dim) l (h-Dim); (b-Dim) (l-Dim) (h-Dim); (b-Dim) (l-Dim) h]; % [x y z] Eckpunkte der Träger in Vektorschreibweise
 patch('Faces', fac,'Vertices',vert8,'FaceColor','g');            % Erzeugen der Gefärbten Flächen (Polygonen)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+if b > 13
+   SM= (b/2) -(Dim/2);           %Hilfsgrösse% Pos X (also Stützenabstand) der Stüzte. Einmitung auf Achsmass der Stütze
+
+%Stuetze VM
+vert3= [SM 0 0; SM Dim 0; (SM+Dim) Dim 0; (SM+Dim) 0 0; (SM+Dim) Dim (h-Dim); (SM+Dim) 0 (h-Dim); SM 0 (h-Dim); SM Dim (h-Dim)]; % [x y z] Eckpunkte der Träger in Vektorschreibweise
+patch('Faces', fac,'Vertices',vert3,'FaceColor','c');            % Erzeugen der Gefärbten Flächen (Polygonen)
+
+%Stuetze HM
+vert5= [SM (l-Dim) 0; SM l 0; (SM+Dim) l 0; (SM+Dim) (l-Dim) 0; (SM+Dim) l (h-Dim); (SM+Dim) (l-Dim) (h-Dim); SM (l-Dim) (h-Dim); SM l (h-Dim)]; % [x y z] Eckpunkte der Träger in Vektorschreibweise
+patch('Faces', fac,'Vertices',vert5,'FaceColor','c');            % Erzeugen der Gefärbten Flächen (Polygonen)
+end
 
 %Binderi i Stützen und Querbalken
 %Aufbau von der Mittea aus um Anfang und Endabstand Binder-Stütze gleich
 %gross zu halten
 i=-a;
-while i <= (l/2)-2*a;         
+while i <= (l/2)-a;         
       i= i + a;
+   XMH = ((l/2)+i-(Dim/2));      %Hilfsgrösse% Achsabstand der Stützen in While Schleife
+
 %Querbalken 
-vert9= [Dim ((l/2)+i-(Dim/2)) (h-Dim); Dim ((l/2)+i-(Dim/2)) h; Dim (((l/2)+i-(Dim/2))+Dim) h; Dim (((l/2)+i-(Dim/2))+Dim) (h-Dim); (b-Dim) (((l/2)+i-(Dim/2))+Dim) h; (b-Dim) (((l/2)+i-(Dim/2))+Dim) (h-Dim); (b-Dim) ((l/2)+i-(Dim/2)) (h-Dim); (b-Dim) ((l/2)+i-(Dim/2)) h]; % [x y z] Eckpunkte der Träger in Vektorschreibweise
+vert9= [Dim XMH (h-Dim); Dim XMH h; Dim (XMH+Dim) h; Dim (XMH+Dim) (h-Dim); (b-Dim) (XMH+Dim) h; (b-Dim) (XMH+Dim) (h-Dim); (b-Dim) XMH (h-Dim); (b-Dim) XMH h]; % [x y z] Eckpunkte der Träger in Vektorschreibweise
 patch('Faces', fac,'Vertices',vert9,'FaceColor','y');            % Erzeugen der Gefärbten Flächen (Polygonen)
 
 %Stuetze A
-vert10= [0 ((l/2)+i-(Dim/2)) 0; 0 (((l/2)+i-(Dim/2))+Dim) 0; Dim (((l/2)+i-(Dim/2))+Dim) 0; Dim ((l/2)+i-(Dim/2)) 0; Dim (((l/2)+i-(Dim/2))+Dim) (h-Dim); Dim ((l/2)+i-(Dim/2)) (h-Dim); 0 ((l/2)+i-(Dim/2)) (h-Dim); 0 (((l/2)+i-(Dim/2))+Dim) (h-Dim)]; % [x y z] Eckpunkte der Träger in Vektorschreibweise
+vert10= [0 XMH 0; 0 (XMH+Dim) 0; Dim (XMH+Dim) 0; Dim XMH 0; Dim (XMH+Dim) (h-Dim); Dim XMH (h-Dim); 0 XMH (h-Dim); 0 (XMH+Dim) (h-Dim)]; % [x y z] Eckpunkte der Träger in Vektorschreibweise
 patch('Faces', fac,'Vertices',vert10,'FaceColor','y');            % Erzeugen der Gefärbten Flächen (Polygonen)
 
 %Stuetze B
-vert11= [b ((l/2)+i-(Dim/2)) 0; b (((l/2)+i-(Dim/2))+Dim) 0; (b-Dim) (((l/2)+i-(Dim/2))+Dim) 0; (b-Dim) ((l/2)+i-(Dim/2)) 0; (b-Dim) (((l/2)+i-(Dim/2))+Dim) (h-Dim); (b-Dim) ((l/2)+i-(Dim/2)) (h-Dim); b ((l/2)+i-(Dim/2)) (h-Dim); b (((l/2)+i-(Dim/2))+Dim) (h-Dim)]; % [x y z] Eckpunkte der Träger in Vektorschreibweise
+vert11= [b XMH 0; b (XMH+Dim) 0; (b-Dim) (XMH+Dim) 0; (b-Dim) XMH 0; (b-Dim) (XMH+Dim) (h-Dim); (b-Dim) XMH (h-Dim); b XMH (h-Dim); b (XMH+Dim) (h-Dim)]; % [x y z] Eckpunkte der Träger in Vektorschreibweise
 patch('Faces', fac,'Vertices',vert11,'FaceColor','y');            % Erzeugen der Gefärbten Flächen (Polygonen)
+
+if b >13
+    %Stuetze VM   
+vert3= [SM XMH 0; SM (XMH+Dim) 0; (SM+Dim) (XMH+Dim) 0; (SM+Dim) XMH 0; (SM+Dim) (XMH+Dim) (h-Dim); (SM+Dim) XMH (h-Dim); SM XMH (h-Dim); SM (XMH+Dim) (h-Dim)]; % [x y z] Eckpunkte der Träger in Vektorschreibweise
+patch('Faces', fac,'Vertices',vert3,'FaceColor','c');            % Erzeugen der Gefärbten Flächen (Polygonen)
+
+%Laengstraeger M        %Zur Zeit durchlaufend noch keine unterbrücke bei den
+%Sützen oder Binder
+vert1= [SM 0 (h-Dim); (SM+Dim) 0 (h-Dim); (SM+Dim) 0 h; SM 0 h; (SM+Dim) l h; SM l h; SM l (h-Dim); (SM+Dim) l (h-Dim)]; % [x y z] Eckpunkte der Träger in Vektorschreibweise
+fac= [1 2 3 4; 4 3 5 6; 6 7 8 5; 1 2 8 7; 6 7 1 4; 2 3 5 8];    % Welche Eckpunkte werden als eine Polygonfläche eingefärbt
+patch('Faces', fac,'Vertices',vert1,'FaceColor','b');            % Erzeugen der Gefärbten Flächen (Polygonen)
+end
+
 end
 k=l/2;
-while k >= 2*a;         
-      k= k - a;
+while k >= a; 
+       k= k - a;
+   XMV = (k-(Dim/2));           %Hilfsgrösse% Achsabstand der Stütze in While Schleife
+
 %Querbalken 
-vert9= [Dim (k-(Dim/2)) (h-Dim); Dim (k-(Dim/2)) h; Dim ((k-(Dim/2))+Dim) h; Dim ((k-(Dim/2))+Dim) (h-Dim); (b-Dim) ((k-(Dim/2))+Dim) h; (b-Dim) ((k-(Dim/2))+Dim) (h-Dim); (b-Dim) (k-(Dim/2)) (h-Dim); (b-Dim) (k-(Dim/2)) h]; % [x y z] Eckpunkte der Träger in Vektorschreibweise
+vert9= [Dim XMV (h-Dim); Dim XMV h; Dim (XMV+Dim) h; Dim (XMV+Dim) (h-Dim); (b-Dim) (XMV+Dim) h; (b-Dim) (XMV+Dim) (h-Dim); (b-Dim) XMV (h-Dim); (b-Dim) XMV h]; % [x y z] Eckpunkte der Träger in Vektorschreibweise
 patch('Faces', fac,'Vertices',vert9,'FaceColor','y');            % Erzeugen der Gefärbten Flächen (Polygonen)
 
 %Stuetze A
-vert10= [0 (k-(Dim/2)) 0; 0 ((k-(Dim/2))+Dim) 0; Dim ((k-(Dim/2))+Dim) 0; Dim (k-(Dim/2)) 0; Dim ((k-(Dim/2))+Dim) (h-Dim); Dim (k-(Dim/2)) (h-Dim); 0 (k-(Dim/2)) (h-Dim); 0 ((k-(Dim/2))+Dim) (h-Dim)]; % [x y z] Eckpunkte der Träger in Vektorschreibweise
+vert10= [0 XMV 0; 0 (XMV+Dim) 0; Dim (XMV+Dim) 0; Dim XMV 0; Dim (XMV+Dim) (h-Dim); Dim XMV (h-Dim); 0 XMV (h-Dim); 0 (XMV+Dim) (h-Dim)]; % [x y z] Eckpunkte der Träger in Vektorschreibweise
 patch('Faces', fac,'Vertices',vert10,'FaceColor','y');            % Erzeugen der Gefärbten Flächen (Polygonen)
 
 %Stuetze B
-vert11= [b (k-(Dim/2)) 0; b ((k-(Dim/2))+Dim) 0; (b-Dim) ((k-(Dim/2))+Dim) 0; (b-Dim) (k-(Dim/2)) 0; (b-Dim) ((k-(Dim/2))+Dim) (h-Dim); (b-Dim) (k-(Dim/2)) (h-Dim); b (k-(Dim/2)) (h-Dim); b ((k-(Dim/2))+Dim) (h-Dim)]; % [x y z] Eckpunkte der Träger in Vektorschreibweise
+vert11= [b XMV 0; b (XMV+Dim) 0; (b-Dim) (XMV+Dim) 0; (b-Dim) XMV 0; (b-Dim) (XMV+Dim) (h-Dim); (b-Dim) XMV (h-Dim); b XMV (h-Dim); b (XMV+Dim) (h-Dim)]; % [x y z] Eckpunkte der Träger in Vektorschreibweise
 patch('Faces', fac,'Vertices',vert11,'FaceColor','y');            % Erzeugen der Gefärbten Flächen (Polygonen)
+
+if b >13
+    %Stuetze VM
+vert3= [SM XMV 0; SM (XMV+Dim) 0; (SM+Dim) (XMV+Dim) 0; (SM+Dim) XMV 0; (SM+Dim) (XMV+Dim) (h-Dim); (SM+Dim) XMV (h-Dim); SM XMV (h-Dim); SM (XMV+Dim) (h-Dim)]; % [x y z] Eckpunkte der Träger in Vektorschreibweise
+patch('Faces', fac,'Vertices',vert3,'FaceColor','c');            % Erzeugen der Gefärbten Flächen (Polygonen)
 end
+
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 
