2264e988f798b8276122362a12662ee02b00d565
Saeschu <33346813+Saeschu@users.noreply.github.com>
2018-01-22 21:06:28 +0100

    sa

--- Master_Stahlhalle_Matlabarbeit.m
+++ Master_Stahlhalle_Matlabarbeit.m
@@ -715,8 +715,27 @@ end
 %Darstellung: Struktur Feld
 subplot(2,2,1)
 hold on
-axis equal
+
 axis off
+axis equal
+
+%<<<<<<< HEAD
+%<<<<<<< HEAD
+
+axis off
+%=======
+axis off
+axis equal
+%>>>>>>> b49f4288c6e63750b5ed5f0ca01a58d4abcd884c
+%=======
+
+
+axis off
+axis off
+axis equal
+
+%>>>>>>> 78989d9545a632410461627e0bac7927da906626
+
 title('Struktur')
 
 %Darstellung Struktur
